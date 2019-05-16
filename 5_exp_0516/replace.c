1065de9525e82d34ff8c4b088890c81681340bfb
knavit <knavit@outlook.com>
2019-05-16 09:51:08 +0800

    added replace

---
+++ 5_exp_0516/replace.c
@@ -0,0 +1,36 @@
+typdef struct pageitem {
+    int page;
+    int exist;
+    int block;
+} PAGEITEM;
+
+typdef struct table {
+    PAGEITEM item[31];
+    int findPage(int);
+    int replacePage(int, int);
+} TABLE;
+
+int table::findPage(int page) {
+    int block = -1;
+    for (int i = 0; i < 31; i++) {
+        if (item[i].exist == 1 && item[i].block == page) {
+            block = i;
+            break;
+        }
+    }
+    return block;
+}
+
+int table::replacePage(int origin, int target) {
+    int success = 1;
+    int index = findPage(origin);
+    if (index == -1) {
+        success = 0;
+    }
+    else {
+        item[index].page = target;
+    }
+    return success;
+}
+
+
